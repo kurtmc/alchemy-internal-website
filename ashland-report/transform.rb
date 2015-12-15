634903c957ad9e13f8f758584125a7bce2df41a7
Kurt McAlpine <kurt@linux.com>
2015-12-16 11:51:16 +1300

    Put into a method

--- ashland-report/transform.rb
+++ ashland-report/transform.rb
@@ -4,53 +4,60 @@ require 'rubygems'
 require 'json'
 require 'csv'
 
-report_path = "/home/kurt/alchemy-workspace/ashland/Mock Reports/Mock Reports (24).csv"
-ordering_path = "Ordering"
-mapping_path = "Mapping"
-    
-report_csv =  CSV.read(report_path)
-original_titles = report_csv[0]
 
-ordering = File.readlines(ordering_path)
-ordering.map do |line|
-    line.strip!
-end
+def transform(report_path, output_path)
+    ordering_path = "Ordering"
+    mapping_path = "Mapping"
+        
+    report_csv =  CSV.read(report_path)
+    original_titles = report_csv[0]
 
-mapping = JSON.parse(File.read(mapping_path))
-
-# Get new titles
-new_titles = Array.new(mapping.size)
-for i in 0...original_titles.size
-    new_title = mapping[original_titles[i]]
-    new_index = ordering.index(new_title)
-    unless new_title.nil? or new_title.empty?
-        new_titles[new_index] = new_title
+    ordering = File.readlines(ordering_path)
+    ordering.map do |line|
+        line.strip!
     end
-end
 
-# Setup values
-result = Array.new
-result.push(new_titles)
+    mapping = JSON.parse(File.read(mapping_path))
 
-for i in 1...report_csv.size
-    tmp = Array.new(mapping.size)
-    for j in 0...original_titles.size
-        new_title = mapping[original_titles[j]]
+    # Get new titles
+    new_titles = Array.new(mapping.size)
+    for i in 0...original_titles.size
+        new_title = mapping[original_titles[i]]
         new_index = ordering.index(new_title)
-
-        value = ""
-        if report_csv[i].size <= j
-            value = ""
-        else
-            value = report_csv[i][j]
-
-            # TODO hooks
+        unless new_title.nil? or new_title.empty?
+            new_titles[new_index] = new_title
         end
-        tmp[new_index] = value
     end
-    result.push(tmp)
+
+    # Setup values
+    result = Array.new
+    result.push(new_titles)
+
+    for i in 1...report_csv.size
+        tmp = Array.new(mapping.size)
+        for j in 0...original_titles.size
+            new_title = mapping[original_titles[j]]
+            new_index = ordering.index(new_title)
+
+            value = ""
+            if report_csv[i].size <= j
+                value = ""
+            else
+                value = report_csv[i][j]
+
+                # TODO hooks
+            end
+            tmp[new_index] = value
+        end
+        result.push(tmp)
+    end
+
+    CSV.open(output_path, "wb") do |csv|
+        result.each { |row| csv << row }
+    end
 end
 
-CSV.open("ashland-report.csv", "wb") do |csv|
-    result.each { |row| csv << row }
-end
+output_path = "ashland-report.csv"
+report_path = "/home/kurt/alchemy-workspace/ashland/Mock Reports/Mock Reports (24).csv"
+
+transform(report_path, output_path)
