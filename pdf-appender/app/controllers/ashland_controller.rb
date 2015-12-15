7ca7116738c8fe39ccde594b057ee3d8f0403d1e
Kurt McAlpine <kurt@linux.com>
2015-12-16 12:51:59 +1300

    Can now generate Ashland report

--- pdf-appender/app/controllers/ashland_controller.rb
+++ pdf-appender/app/controllers/ashland_controller.rb
@@ -1,22 +1,79 @@
+require 'rubygems'
+require 'json'
+require 'csv'
+
 class AshlandController < ApplicationController
     def new
-        @crm_report = nil
     end
 
     def show
     end
 
     def create
-        uploaded_io = params[:pdf][:pdf_file]
+        uploaded_io = params[:ashland][:csv_file]
         filename = uploaded_io.original_filename
-        File.open(Rails.root.join('public', 'uploads', filename), 'wb') do |file|
+        report_path = Rails.root.join('public', 'uploads', filename)
+        File.open(report_path, 'wb') do |file|
             file.write(uploaded_io.read)
         end
-        #redirect_to pdfs_path
-        cmd = 'cd footer_pdf; ./run.sh "../public/uploads/' + filename + '"'
-        value = `#{cmd}`
-        #render plain: cmd + ':::::::' + value
-        #render plain: `pwd`
-        send_file(File.join(Rails.root, "footer_pdf", "output", filename))
+
+        output_path = Rails.root.join("public", "uploads", "ashland-report.csv")
+
+        transform(report_path, output_path)
+
+        send_file(output_path)
     end
+
+    def transform(report_path, output_path)
+        ordering_path = Rails.root.join("ashland-report", "Ordering")
+        mapping_path = Rails.root.join("ashland-report", "Mapping")
+            
+        report_csv =  CSV.read(report_path)
+        original_titles = report_csv[0]
+
+        ordering = File.readlines(ordering_path)
+        ordering.map do |line|
+            line.strip!
+        end
+
+        mapping = JSON.parse(File.read(mapping_path))
+
+        # Get new titles
+        new_titles = Array.new(mapping.size)
+        for i in 0...original_titles.size
+            new_title = mapping[original_titles[i]]
+            new_index = ordering.index(new_title)
+            unless new_title.nil? or new_title.empty?
+                new_titles[new_index] = new_title
+            end
+        end
+
+        # Setup values
+        result = Array.new
+        result.push(new_titles)
+
+        for i in 1...report_csv.size
+            tmp = Array.new(mapping.size)
+            for j in 0...original_titles.size
+                new_title = mapping[original_titles[j]]
+                new_index = ordering.index(new_title)
+
+                value = ""
+                if report_csv[i].size <= j
+                    value = ""
+                else
+                    value = report_csv[i][j]
+
+                    # TODO hooks
+                end
+                tmp[new_index] = value
+            end
+            result.push(tmp)
+        end
+
+        CSV.open(output_path, "wb") do |csv|
+            result.each { |row| csv << row }
+        end
+    end
+
 end
