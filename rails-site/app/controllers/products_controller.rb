1f9ceeee1095186523bd0f5952b6962a892a3724
Kurt McAlpine <kurt@linux.com>
2016-01-07 15:43:06 +1300

    Show buttons working

--- rails-site/app/controllers/products_controller.rb
+++ rails-site/app/controllers/products_controller.rb
@@ -6,27 +6,35 @@ class ProductsController < ApplicationController
         products_csv = get_product_csv
         prods = Array.new
         products_csv.each { |x|
-            prods << Product.new(product_id:x[0], description:x[4])
+            prods << new_product(x)
         }
         @products = prods
     end
 
     def show
-        products_csv = get_product_csv
-        products_csv.each { |x|
-            if x[0] == params[:id] then
-                @product = Product.new(product_id:x[0], description:x[4])
-                return
-            end
-        }
+        @product = find_product(params[:id])
     end
 
     private
+
+    def new_product(csv_entry)
+        return Product.new(product_id:csv_entry[0], description:csv_entry[4])
+    end
     
     def get_product_csv
         products_csv_path = Rails.root.join('alchemy-info-tables', 'gen', 'NZ_ID_SDS_PDS_VENDOR_NAME.csv')
         products_csv = CSV.read(products_csv_path)
         titles = products_csv.shift
         return products_csv
+    end
+
+    def find_product(product_id)
+        products_csv = get_product_csv
+        products_csv.each { |x|
+            if x[0] == params[:id] then
+                return new_product(x)
+            end
+        }
+        return nil
     end
 end
