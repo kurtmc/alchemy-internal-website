0b8dae2b4fe67385123548e7aaf96de2ef6536e2
Kurt McAlpine <kurt@linux.com>
2016-01-07 15:27:23 +1300

    Shows products

--- rails-site/app/controllers/products_controller.rb
+++ rails-site/app/controllers/products_controller.rb
@@ -3,13 +3,30 @@ require 'csv'
 
 class ProductsController < ApplicationController
     def index
-        products_csv_path = Rails.root.join('alchemy-info-tables', 'gen', 'NZ_ID_SDS_PDS_VENDOR_NAME.csv')
-        products_csv = CSV.read(products_csv_path)
-        titles = products_csv.shift
+        products_csv = get_product_csv
         prods = Array.new
         products_csv.each { |x|
             prods << Product.new(product_id:x[0], description:x[4])
         }
         @products = prods
+    end
+
+    def show
+        products_csv = get_product_csv
+        products_csv.each { |x|
+            if x[0] == params[:id] then
+                @product = Product.new(product_id:x[0], description:x[4])
+                return
+            end
+        }
+    end
+
+    private
+    
+    def get_product_csv
+        products_csv_path = Rails.root.join('alchemy-info-tables', 'gen', 'NZ_ID_SDS_PDS_VENDOR_NAME.csv')
+        products_csv = CSV.read(products_csv_path)
+        titles = products_csv.shift
+        return products_csv
     end
 end
