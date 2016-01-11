f7456ea19abaaa02ebcbaf3d2a98ed705ab66813
Kurt McAlpine <kurt@linux.com>
2016-01-11 16:53:27 +1300

    If just one file is added is is not zip

--- rails-site/app/controllers/utils/file_process_utils.rb
+++ rails-site/app/controllers/utils/file_process_utils.rb
@@ -10,28 +10,40 @@ class FileProcessUtils
             # do nothing
         end
         new_filenames = Array.new
-        input_files.each { |uploaded_io|
-            # Write uploaded files
-            filename = uploaded_io.original_filename
-            File.open(Rails.root.join('public', 'uploads', filename), 'wb') do |file|
-                file.write(uploaded_io.read)
-            end
+        if input_files.length == 1
+                uploaded_io = input_files[0]
+                filename = uploaded_io.original_filename
+                File.open(Rails.root.join('public', 'uploads', filename), 'wb') do |file|
+                    file.write(uploaded_io.read)
+                end
+                # Process files
+                file_processor.call(filename)
+                output_path = "#{output_directory}/#{filename}"
+                app_controller.send_file(File.join(output_path))
+        else
+            input_files.each { |uploaded_io|
+                # Write uploaded files
+                filename = uploaded_io.original_filename
+                File.open(Rails.root.join('public', 'uploads', filename), 'wb') do |file|
+                    file.write(uploaded_io.read)
+                end
 
-            # Process files
-            file_processor.call(filename)
-            new_filenames << filename
-        }
+                # Process files
+                file_processor.call(filename)
+                new_filenames << filename
+            }
 
-        Zip::File.open(zipfile_path, Zip::File::CREATE) do |zipfile|
-            new_filenames.each do |filename|
-                # Two arguments:
-                # - The name of the file as it will appear in the archive
-                # - The original file, including the path to find it
-                file_path = "#{output_directory}/#{filename}"
-                zipfile.add(filename, file_path)
+            Zip::File.open(zipfile_path, Zip::File::CREATE) do |zipfile|
+                new_filenames.each do |filename|
+                    # Two arguments:
+                    # - The name of the file as it will appear in the archive
+                    # - The original file, including the path to find it
+                    file_path = "#{output_directory}/#{filename}"
+                    zipfile.add(filename, file_path)
+                end
             end
+            # Send files
+            app_controller.send_file(File.join(zipfile_path))
         end
-        # Send files
-        app_controller.send_file(File.join(zipfile_path))
     end
 end
