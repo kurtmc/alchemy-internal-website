a66d4ae5952796b0a81ecdfa35d3777d1863191d
Kurt McAlpine <kurt@linux.com>
2016-01-15 12:16:32 +1300

    Removing credentials from repo

--- rails-site/start.sh
+++ rails-site/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 source ~/.rvm/scripts/rvm
+DATABASE_CONFIG=database.yml
+if [ ! -f $DATABASE_CONFIG ]; then
+	    echo "$DATABASE_CONFIG not found!"
+	    exit 1
+fi
+cp $DATABASE_CONFIG config/
 
 rvm use 2.1
 rvm gemset use rails41
