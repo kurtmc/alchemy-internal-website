498da08307a4d0ed320f62f4c080669df5b7c0c6
Kurt McAlpine <kurt@linux.com>
2016-01-14 15:46:17 +1300

    Basic customer view, missing fields that are difficult to find

--- rails-site/app/controllers/customers_controller.rb
+++ rails-site/app/controllers/customers_controller.rb
@@ -2,4 +2,8 @@ class CustomersController < ApplicationController
     def index
         @customers = Customer.all
     end
+
+    def show
+        @customer = Customer.find_by customer_id: params[:id]
+    end
 end
