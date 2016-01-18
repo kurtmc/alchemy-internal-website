48f6a9870a1b1271878b058ea1a8dbfc967bb507
Kurt McAlpine <kurt@linux.com>
2016-01-18 15:34:28 +1300

    Running server with passenger

--- rails-site/start.sh
+++ rails-site/start.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-source ~/.rvm/scripts/rvm
-DATABASE_CONFIG=database.yml
-if [ ! -f $DATABASE_CONFIG ]; then
-	    echo "$DATABASE_CONFIG not found!"
-	    exit 1
-fi
-cp $DATABASE_CONFIG config/
-
-rvm use 2.1
-rvm gemset use rails41
-rvmsudo rails s --binding=0.0.0.0 -p 80
+bundle exec passenger start
