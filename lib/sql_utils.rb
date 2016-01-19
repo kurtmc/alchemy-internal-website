46f998033dcc937d31b2c8812919a7f53928c8d2
Kurt McAlpine <kurt@linux.com>
2016-01-20 12:08:10 +1300

    More date utilities

--- lib/sql_utils.rb
+++ lib/sql_utils.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 module SqlUtils
     def self.escape(value)
         return "#{ActiveRecord::Base.connection.quote(value)}"
@@ -7,9 +9,34 @@ module SqlUtils
         return Navision.connection.select_all(sql)
     end
 
-    def self.sql_date(year, month, day)
-        date = Date.new(year, month, day)
+    def self.sql_date(date)
         return "CAST('#{date.strftime("%Y%m%d %H:%M:00.000")}' AS DATETIME)"
+    end
+
+    def self.date_range(column, start_date, end_date)
+        return "#{column} >= #{self.sql_date(start_date)} and #{column} <= #{self.sql_date(end_date)}"
+    end
+
+    def self.beginning_financial_year(date = nil)
+        if date.nil?
+            date = Time.now
+        end
+        if date >= Date.new(date.year, 4, 1)
+            return Date.new(date.year, 4, 1)
+        else
+            return Date.new(date.year - 1, 4, 1)
+        end
+    end
+
+    def self.ending_financial_year(date = nil)
+        if date.nil?
+            date = Time.now
+        end
+        if date < Date.new(date.year, 3, 31)
+            return Date.new(date.year, 3, 31)
+        else
+            return Date.new(date.year + 1, 3, 31)
+        end
     end
 
 end
