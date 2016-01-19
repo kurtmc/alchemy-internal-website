a2a832a1afc3ed7c648dc2f6dd5d30604857b889
Kurt McAlpine <kurt@linux.com>
2016-01-20 11:33:10 +1300

    Method to generate sql date

--- lib/sql_utils.rb
+++ lib/sql_utils.rb
@@ -6,4 +6,10 @@ module SqlUtils
     def self.execute_sql(sql)
         return Navision.connection.select_all(sql)
     end
+
+    def self.sql_date(year, month, day)
+        date = Date.new(year, month, day)
+        return "CAST('#{date.strftime("%Y%m%d %H:%M:00.000")}' AS DATETIME)"
+    end
+
 end
