ddf341585091d4b433e09368d64991825634f69f
Kurt McAlpine <kurt@linux.com>
2016-01-21 15:10:29 +1300

    Admin can only add users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+
+    # restrict access to admin module for non-admin users
+    def authenticate_admin_user!
+        if current_user.nil?
+            raise SecurityError
+            return
+        end
+        unless current_user.admin == true
+            raise SecurityError
+            return
+        end
+    end
+
+    rescue_from SecurityError do |exception|
+        redirect_to root_url
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121005243) do
+ActiveRecord::Schema.define(version: 20160121015113) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 20160121005243) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
