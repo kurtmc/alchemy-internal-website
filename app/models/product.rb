adf48cd891594955ea660445f123bd3cd6cae110
Kurt McAlpine <kurt@linux.com>
2016-01-22 14:02:02 +1300

    Loading all products from navision

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,15 @@ require 'uri'
 
 class Product < ActiveRecord::Base
     include SqlUtils
+
+    def self.documents_path
+        return Rails.root.join('alchemy-info-tables', 'res', 'Product_Information')
+    end
+
+    def absolute_documents_path
+        return Product.documents_path.join(self.directory)
+    end
+
     def get_product_id
         return self.product_id.gsub('/',"%2F").gsub('%', "%25")
     end
@@ -89,6 +98,41 @@ class Product < ActiveRecord::Base
         return prod
     end
 
+    def self.new_product_from_record(record)
+        # Find old product or create new
+        product = Product.find_by(product_id: record['No_'])
+        if product.nil?
+            product = Product.new
+            product.product_id = record['No_']
+        end
+
+        # Hackery so that we can map product_id to direcories in the filesystem
+        product.directory = record['No_'].gsub('/',"_") 
+
+        docs = Dir.glob(self.documents_path.join(product.directory, '*'))
+        docs = docs.map { |x| File.basename(x) }
+        docs.each { |doc|
+            if doc.start_with?('SDS - ')
+                product.sds = doc
+            elsif doc.start_with?('PDS - ')
+                product.pds = doc
+            end
+        }
+
+        product.vendor_id = record['Vendor No_']
+        product.vendor_name = record['Vendor Name']
+        product.description = record['Description']
+        product.description2 = record['Description']
+        product.sds_expiry = record['SDS Expiry Date']
+        product.unit_measure = record['Base Unit of Measure']
+        product.shelf_life = record['Shelf No_']
+        product.inventory = record['Inventory']
+		product.quantity_purchase_order = record['Quantity Purchase Order']
+		product.quantity_packing_slip = record['Quantity Packing Slip']
+
+		product.save
+    end
+
     def self.load_all
         csv_path = Rails.root.join('alchemy-info-tables', 'gen', 'NZ_ID_SDS_PDS_VENDOR_NAME.csv')
         prods = Array.new
@@ -109,6 +153,39 @@ class Product < ActiveRecord::Base
                 prod.save
             end
         end
+    end
+
+    def self.load_all_from_nav
+        sql = "
+			SELECT
+				item.*,
+				vendor.\"Name\" AS \"Vendor Name\",
+				inventory.\"Inventory\",
+				purchase.Quantity AS \"Quantity Purchase Order\",
+				sales.Quantity AS \"Quantity Packing Slip\"
+			FROM
+				NAVLIVE.dbo.\"Alchemy Agencies Ltd$Item\" AS item
+				LEFT JOIN NAVLIVE.dbo.\"Alchemy Agencies Pty Ltd$Vendor\" AS vendor
+				ON item.\"Vendor No_\" = vendor.No_
+				LEFT JOIN (
+					SELECT
+						\"Item No_\",
+						SUM(\"Quantity\") AS \"Inventory\"
+					FROM
+						NAVLIVE.dbo.\"Alchemy Agencies Ltd$Item Ledger Entry\"
+					GROUP BY
+						\"Item No_\"
+				) inventory
+				ON inventory.\"Item No_\" = item.No_
+				LEFT JOIN NAVLIVE.dbo.\"Alchemy Agencies Ltd$Purchase Line\" as purchase
+				ON item.No_ = purchase.No_
+				LEFT JOIN NAVLIVE.dbo.\"Alchemy Agencies Ltd$Sales Line\" as sales
+				ON item.No_ = sales.No_
+        "
+        records = SqlUtils.execute_sql(sql)
+        records.each { |record|
+            new_product_from_record(record)
+        }
     end
 
 end
