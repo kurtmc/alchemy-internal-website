490d8bacfa2c444da40d120d7df07f38a711ba48
Kurt McAlpine <kurt@linux.com>
2016-01-26 15:39:23 +1300

    Can set other users ad admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,28 +1,40 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+    permit_params :email, :password, :password_confirmation, :admin
 
-  index do
-    selectable_column
-    id_column
-    column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
-    actions
-  end
-
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
-
-  form do |f|
-    f.inputs "Admin Details" do
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
+    index do
+        selectable_column
+        id_column
+        column :email
+        column :current_sign_in_at
+        column :sign_in_count
+        column :created_at
+        actions
     end
-    f.actions
-  end
+
+    filter :email
+    filter :current_sign_in_at
+    filter :sign_in_count
+    filter :created_at
+
+    form do |f|
+        f.inputs "Admin Details" do
+            f.input :email
+            f.input :password
+            f.input :password_confirmation
+            f.input :admin 
+        end
+        f.actions
+    end
+
+    controller do
+        def update
+            if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+                params[:user].delete("password")
+                params[:user].delete("password_confirmation")
+            end
+            super
+        end
+    end
 
 end
+
