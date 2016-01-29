a26586aefbf57fae0bc88537d84d18a041c00c42
Kurt McAlpine <kurt@linux.com>
2016-01-29 16:28:18 +1300

    Added email credentails

--- config/application.rb
+++ config/application.rb
@@ -32,13 +32,10 @@ module PdfAppender
         }
         config.action_mailer.delivery_method = :smtp
         config.action_mailer.smtp_settings = {
-            address: "mail.example.com",
-            port: 110, #I'm not sure if the port need to be 25 or 110... it depends of which port your Exchange listens, usually 25, but there are strong recommendations to use 110, letting port 25 free for smtp communication between smtp servers only.
-            domain: "my.domainame.com",
-            authentication: :ntlm,
-            enable_starttls_auto: true,
-            user_name: "my_username",
-            password: "my_password"
+            address: "smtp.optimus.co.nz",
+            port: 25,
+            domain: "optimus.co.nz",
+            openssl_verify_mode: 'none'
         }
 
     end
