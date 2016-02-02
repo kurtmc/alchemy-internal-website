9b4b3b2dea0e341af3fd8ba327b23763a2d54a9b
Kurt McAlpine <kurt@linux.com>
2016-02-03 09:03:27 +1300

    Fixed but in email controller

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -6,12 +6,13 @@ class EmailController < ApplicationController
     end
 
     def create
+        @product = Product.find(params[:product_id])
         from = current_user.email
         to = params[:email][:to]
         subject = params[:email][:subject]
         body = params[:email][:body]
         unless params[:product_id].nil?
-            p = Product.find(params[:product_id])
+            p = @product
             mail = AlchemyMailer.send_with_attachment(from, to, subject, body, p.sds, p.absolute_documents_path.join(p.sds))
         else
             mail = AlchemyMailer.send_full_email(from, to, subject, body)
