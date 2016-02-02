28c6a32b2b922cdbba9820e66b672a25e096c327
Kurt McAlpine <kurt@linux.com>
2016-02-02 14:04:26 +1300

    Can attach sds to email

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -9,10 +9,18 @@ class EmailController < ApplicationController
         to = params[:email][:to]
         subject = params[:email][:subject]
         body = params[:email][:body]
-        mail = AlchemyMailer.send_full_email(from, to, subject, body)
+        puts 'params =================================='
+        puts params.inspect
+        unless params[:product_id].nil?
+            p = Product.find(params[:product_id])
+            mail = AlchemyMailer.send_with_attachment(from, to, subject, body, p.sds, p.absolute_documents_path.join(p.sds))
+        else
+            mail = AlchemyMailer.send_full_email(from, to, subject, body)
+        end
         mail.deliver!
 
-        render 'new'
+        flash.now[:notice] = "Email sent successfully!"
+        render :new
     end
 
 end
--- app/mailers/alchemy_mailer.rb
+++ app/mailers/alchemy_mailer.rb
@@ -8,4 +8,9 @@ class AlchemyMailer < ActionMailer::Base
     def send_full_email(from, to, subject, body)
         mail(from: from, to: to, subject: subject, body: body)
     end
+
+    def send_with_attachment(from, to, subject, body, attachment_name, attachment_path)
+        attachments[attachment_name] = File.read(attachment_path)
+        mail(from: from, to: to, subject: subject, body: body)
+    end
 end
