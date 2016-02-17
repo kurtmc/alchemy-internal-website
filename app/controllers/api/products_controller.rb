8f14345886ec036213223ba8e818562f0a73b190
Kurt McAlpine <kurt@linux.com>
2016-02-17 18:12:39 +1300

    Updated products json, to include documents and document type

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,40 +4,15 @@ class Api::ProductsController < Api::ApiController
         @products = Product.all.order :product_id
         respond_to do |format|
             format.json {
-                if params[:csv] == 'true'
-                    render text: get_products_csv
-                else
-                    render json: @products
-                end
+                    render :json => @products.to_json(:include => {:documents => {:include => :document_type}})
             }
         end
     end
 
     def show
         respond_to do |format|
-            format.json { render json: Product.find(params[:id])}
+            format.json { render :json => Product.find(params[:id]).to_json(:include => {:documents => {:include => :document_type}}) }
         end
     end
 
-    def get_products_csv
-        mapping = Hash.new
-        mapping['id'] = 'id'
-        mapping['product_id'] = 'product_id'
-        mapping['directory'] = 'directory'
-        mapping['sds'] = 'sds'
-        mapping['pds'] = 'pds'
-        mapping['vendor_id'] = 'vendor_id'
-        mapping['vendor_name'] = 'vendor_name'
-        mapping['description'] = 'product_name'
-        mapping['description2'] = 'pack_size'
-        mapping['new_description'] = 'description'
-        
-        csv_string = CSV.generate do |csv|
-            csv << mapping.values
-            all_products = Product.select(mapping.keys)
-            all_products.each do |product|
-                csv << product.attributes.values
-            end
-        end
-    end
 end
