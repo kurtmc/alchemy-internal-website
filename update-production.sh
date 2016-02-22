d55bb638a6ace6ce9a8cc5d77789ded44b9f3892
Kurt McAlpine <kurt@linux.com>
2016-02-22 21:04:06 +1300

    Update submodules when updating production

--- update-production.sh
+++ update-production.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git pull
+git submodule update --init --recursive
 rvm use ruby-2.1.7
 bundle install --deployment --without development test
 bundle exec rake assets:precompile db:migrate RAILS_ENV=production
