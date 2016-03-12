f2fae8dbe6e36c8e60055eac7d353046e13f33b4
Kurt McAlpine <kurt@linux.com>
2016-03-12 13:53:12 +1300

    It's super basic but it kinda works

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,9 +1,21 @@
 var ready;
 ready = function() {
-	$('.wysihtml5').each(function(i, elem) {
-		$(elem).wysihtml5();
-	});
-	$('.dropdown-toggle').dropdown();
+  $('.wysihtml5').each(function(i, elem) {
+    $(elem).wysihtml5({'toolbar': {
+      'font-styles': true,
+      'color': true,
+      'emphasis': {
+        'small': true
+      },
+      'blockquote': true,
+      'lists': true,
+      'html': false,
+      'link': true,
+      'image': true,
+      'smallmodals': false
+    }});
+  });
+  $('.dropdown-toggle').dropdown();
 }
 
 $(document).ready(ready);
