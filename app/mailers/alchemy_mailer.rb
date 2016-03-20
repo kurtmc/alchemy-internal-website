2a5ac983e8eb4d77fc674e22e15d7af44cddb31d
Kurt McAlpine <kurt@linux.com>
2016-03-20 16:14:18 +1300

    Changes to emailer, not currently working

--- app/mailers/alchemy_mailer.rb
+++ app/mailers/alchemy_mailer.rb
@@ -11,6 +11,9 @@ class AlchemyMailer < ActionMailer::Base
 
     def send_with_attachment(from, to, subject, body, attachment_name, attachment_path)
         attachments[attachment_name] = File.read(attachment_path)
-        mail(from: from, to: to, subject: subject, body: body, content_type:  "text/html")
+	@body = body
+        mail(from: from, to: to, subject: subject ) do |format|
+		format.html { render layout: 'send_full_email' }
+	end
     end
 end
