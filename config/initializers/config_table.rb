77d9e13eda983c99863f9fb313570f14ac3da9c4
joshofreeness <joshofreeness@gmail.com>
2016-08-15 22:29:01 +1200

    Initialiser now safe to run on unmigrated databases

--- config/initializers/config_table.rb
+++ config/initializers/config_table.rb
@@ -1,4 +1,8 @@
 class Helper
+	def self.check_db
+		return ActiveRecord::Base.connection.table_exists? 'config_tables'
+	end
+
 	def self.setup(key, value)
 		default = ConfigTable.new
 		default.key = key
@@ -7,8 +11,10 @@ class Helper
 	end
 end
 
-Helper.setup('Terms of Use', 'Terms of Use need to be updated by an administrator.')
-#As needed more config values and a default value can be added here
-#Below is an example of usage
-#Both key and value should be user friendly
-#Helper.setup('Terms of Sale', 'Please insert terms of sale')
+if Helper.check_db
+	Helper.setup('Terms of Use', 'Terms of Use need to be updated by an administrator.')
+	#As needed more config values and a default value can be added here
+	#Below is an example of usage
+	#Both key and value should be user friendly
+	#Helper.setup('Terms of Sale', 'Please insert terms of sale')
+end
