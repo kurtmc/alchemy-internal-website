dec4be3acabbb1dcaba7f2ff8f8d7923ad63685a
Kurt McAlpine <kurt@linux.com>
2016-08-17 21:32:45 +1200

    Fixed syntax error in migration

--- db/migrate/20160813231545_create_config_tables.rb
+++ db/migrate/20160813231545_create_config_tables.rb
@@ -1,7 +1,7 @@
 class CreateConfigTables < ActiveRecord::Migration
   def change
     create_table :config_tables do |t|
-      t.string :key :index
+      t.string :key
       t.text :value
 
       t.timestamps
